b2612cd798bb69419b93e652ab6bb79b44ce408c
ishaan <santhanakumar.sankaranarayanan@ust-global.com>
2020-04-30 18:32:27 +0530

    initial commit springboot application with basic config

---
+++ onlinebooks/src/main/java/com/ust/springboot/onlinebooks/OnlinebooksApplication.java
@@ -0,0 +1,13 @@
+package com.ust.springboot.onlinebooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebooksApplication.class, args);
+	}
+
+}
